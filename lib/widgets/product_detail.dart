378f22098da396b690668cb32f69b4fa3a46bd59
FelipeLara00 <felipe.lara@uc.cl>
2023-12-20 21:10:13 -0300

    feat: tabBar evaluacion nutricionistas

--- lib/widgets/product_detail.dart
+++ lib/widgets/product_detail.dart
@@ -4,134 +4,165 @@ class ProductDetail extends StatelessWidget {
   final dynamic data; // Objeto con atributos variables
 
   const ProductDetail({
-    super.key, 
+    Key? key, 
     required this.data,
-  });
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Dialog(
-      insetPadding: const EdgeInsets.all(20),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(25),
-      ),
-      elevation: 10,
-      child: ConstrainedBox(
-        constraints: BoxConstraints(
-          maxHeight: MediaQuery.of(context).size.height * 0.9,
+    return DefaultTabController(
+      length: 2, // Número de pestañas (Nutricionistas y Tabla Nutricional)
+      child: Dialog(
+        insetPadding: const EdgeInsets.all(20),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(25),
         ),
-        child: Padding(
-          padding: const EdgeInsets.all(15),
+        elevation: 10,
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            maxHeight: MediaQuery.of(context).size.height * 0.9,
+          ),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Image.network(
-                    data["basicInformation"]["photoUrl"],
-                    height: 80,
-                    width: 80,
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 20.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            data["basicInformation"]["description"],
-                            style: const TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.bold,
-                            ),
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 2,
-                          ),
-                          Text(
-                            data["basicInformation"]["brands"][0]["name"],
-                            style: const TextStyle(
-                              fontSize: 14,
-                            ),
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 3,
-                          ),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Image.network(
+                        data["basicInformation"]["photoUrl"],
+                        height: 80,
+                        width: 80,
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 20.0),
+                          child: Column(
                             mainAxisAlignment: MainAxisAlignment.start,
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Row(
-                                children: [
-                                  Image.asset(
-                                    'lib/assets/Estrella_completa.png',
-                                    height: 20,
-                                    width: 20,
-                                  ),
-                                  Image.asset(
-                                    'lib/assets/Estrella_completa.png',
-                                    height: 20,
-                                    width: 20,
-                                  ),
-                                  Image.asset(
-                                    'lib/assets/Estrella_completa.png',
-                                    height: 20,
-                                    width: 20,
-                                  ),
-                                  Image.asset(
-                                    'lib/assets/Estrella_completa.png',
-                                    height: 20,
-                                    width: 20,
-                                  ),
-                                ],
-                              ),
-                              const Text(
-                                "Evaluado por 7 nutricionistas",
-                                style: TextStyle(
-                                  fontSize: 10,
+                              Text(
+                                data["basicInformation"]["description"],
+                                style: const TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
                                 ),
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 2,
                               ),
-                            ]
-                          ) 
-                        ],
+                              Text(
+                                data["basicInformation"]["brands"][0]["name"],
+                                style: const TextStyle(
+                                  fontSize: 14,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 3,
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    children: [
+                                      Image.asset(
+                                        'lib/assets/Estrella_completa.png',
+                                        height: 20,
+                                        width: 20,
+                                      ),
+                                      Image.asset(
+                                        'lib/assets/Estrella_completa.png',
+                                        height: 20,
+                                        width: 20,
+                                      ),
+                                      Image.asset(
+                                        'lib/assets/Estrella_completa.png',
+                                        height: 20,
+                                        width: 20,
+                                      ),
+                                      Image.asset(
+                                        'lib/assets/Estrella_completa.png',
+                                        height: 20,
+                                        width: 20,
+                                      ),
+                                    ],
+                                  ),
+                                  const Text(
+                                    "Evaluado por 7 nutricionistas",
+                                    style: TextStyle(
+                                      fontSize: 10,
+                                    ),
+                                  ),
+                                ]
+                              ) 
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                const Text(
+                  "Evaluación Nutricional",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+
+                // TabBar para cambiar entre Nutricionistas y Tabla Nutricional
+                const TabBar(
+                  indicatorSize: TabBarIndicatorSize.tab,
+                  indicatorColor: Color(0xFF76FDB1),
+                  labelColor: Colors.black,
+                  indicatorWeight: 8.0,
+                  labelPadding: EdgeInsets.symmetric(horizontal: 1.0),
+                  tabs: [
+                    Tab(
+                      child: Text(
+                        "Nutricionistas",
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
                       ),
                     ),
-                  )
-                ],
-              ),
-              const SizedBox(
-                height: 25,
-              ),
-              const Text(
-                "Evaluación Nutricional",
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+                    Tab(
+                      child: Text(
+                        "Tabla Nutricional",
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Text(
-                    "Nutricionistas",
-                    style: TextStyle(
-                      fontSize: 18,
-                    ),
+
+                // Contenido de las pestañas
+                const Expanded(
+                  child: TabBarView(
+                    children: [
+                      // Contenido de la pestaña Nutricionistas
+                      Center(
+                        child: Text("Contenido Nutricionistas"),
+                      ),
+
+                      // Contenido de la pestaña Tabla Nutricional
+                      Center(
+                        child: Text("Contenido Tabla Nutricional"),
+                      ),
+                    ],
                   ),
-                  Text(
-                    "Tabla Nutricional",
-                    style: TextStyle(
-                      fontSize: 18,
-                    ),
-                  ),
-                ],
-              )
-            ],
-          ),
+                ),
+              ],
+            ),
+          
         ),
       ),
     );
