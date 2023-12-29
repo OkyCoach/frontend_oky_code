b8e5adc1cc5024e4f3db48ee60e65cd93969e18e
FelipeLara00 <felipe.lara@uc.cl>
2023-12-29 14:16:11 -0300

    fix: error de ajuste al tamaño

--- lib/widgets/recommended.dart
+++ lib/widgets/recommended.dart
@@ -11,23 +11,23 @@ class Recommended extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
-    double containerHeight = screenHeight * 0.4;
+    double containerHeight = screenHeight * 0.25;
     double containerWidth = MediaQuery.of(context).size.width;
 
     // Calcula el ancho para cada elemento dividiendo el ancho total entre el número total de elementos (4 en este caso).
-    double itemWidth = containerWidth / 4.5;
+    double itemWidth = containerWidth / 3;
 
     return Container(
       height: containerHeight,
       color: const Color(0xFFFFFFFF),
       child: Padding(
-        padding: const EdgeInsets.all(20.0),
+        padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const Padding(
-              padding: EdgeInsets.only(bottom: 15),
+              padding: EdgeInsets.only(bottom: 5),
               child: Text(
                 "Te recomendamos",
                 style: TextStyle(
@@ -37,16 +37,19 @@ class Recommended extends StatelessWidget {
                 ),
               ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                for (int i = 0; i < 4; i++)
-                  SizedBox(
+            SizedBox(
+              height: containerHeight * 0.7,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 4,
+                itemBuilder: (context, index) {
+                  return SizedBox(
                     width: itemWidth,
                     child: buildProduct(),
-                  ),
-              ],
-            )
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),
@@ -55,22 +58,22 @@ class Recommended extends StatelessWidget {
 
   Widget buildProduct() {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center, // Centra la imagen horizontalmente
+      mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Container(
-          alignment: Alignment.center, // Centra la imagen horizontalmente
+          alignment: Alignment.center,
           child: Image.network(
             "https://static.okto.shop/img/products/s/69/64/696474485fb0343a15488288df6a0bebff0119ec_11917.jpg",
-            height: 60,
-            width: 60,
+            height: 50,
+            width: 50,
           ),
         ),
-        const SizedBox(height: 8), // Espacio entre la imagen y el primer texto
+        const SizedBox(height: 8),
         const Text(
           "Galletitas de arroz Arándano",
           style: TextStyle(
-            fontSize: 12,
+            fontSize: 10,
             fontWeight: FontWeight.w600,
             color: Color(0xFF201547),
           ),
@@ -80,7 +83,7 @@ class Recommended extends StatelessWidget {
         const Text(
           "MIZOS",
           style: TextStyle(
-            fontSize: 13,
+            fontSize: 10,
             fontWeight: FontWeight.normal,
             color: Color(0xFF201547),
           ),
