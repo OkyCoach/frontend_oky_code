bb3d3dab83de9c11454b1a8e5000a98fbe7f49e7
FelipeLara00 <felipe.lara@uc.cl>
2024-04-11 13:16:28 -0400

    nuevo scanner funciona

--- lib/widgets/new_scanner.dart
+++ lib/widgets/new_scanner.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:frontend_oky_code/widgets/product_popup.dart';
 import 'package:frontend_oky_code/widgets/not_found_popup.dart';
+import 'package:frontend_oky_code/widgets/no_evaluation_popup.dart';
+import 'package:frontend_oky_code/helpers/fetch_data.dart';
 
 class MyScannerWidget extends StatefulWidget {
   @override
@@ -9,6 +12,12 @@ class MyScannerWidget extends StatefulWidget {
 
 class _MyScannerWidgetState extends State<MyScannerWidget> {
   bool scanning = false;
+
+  Future<Map<String, dynamic>> _didScan(String? barcode) async {
+    var product = await fetchBarcodeData(barcode);
+    var evaluation = await fetchEvaluationData(barcode);
+    return {'product': product, 'evaluation': evaluation};
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,19 +29,27 @@ class _MyScannerWidgetState extends State<MyScannerWidget> {
           // torchEnabled: false,
           returnImage: false,
         ),
-        onDetect: (capture) {
+        onDetect: (capture) async {
           if (!scanning) {
             setState(() {
               scanning = true;
             });
 
             final List<Barcode> barcodes = capture.barcodes;
+            var scanResult = await _didScan(barcodes[0].rawValue);
+
             showDialog(
               barrierColor: Colors.white.withOpacity(0),
               context: context,
               builder: (_) {
-                return NotFoundPopup(barcode: "3483494");
-              },
+                if (scanResult["product"]["barcode"] != null && scanResult["evaluation"]["puntos_obtenidos"] != null) {
+                  return ProductPopup(product: scanResult["product"], evaluation: scanResult["evaluation"], canScan: true,);
+                } else if (scanResult["product"]["barcode"] != null && scanResult["evaluation"]["puntos_obtenidos"] == null) {
+                  return NoEvaluationPopup(product: scanResult["product"]);
+                } else {
+                  return NotFoundPopup(barcode: barcodes[0].rawValue);
+                }
+              }
             ).then((_) {
               setState(() {
                 scanning = false;
