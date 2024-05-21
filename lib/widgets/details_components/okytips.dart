570f0393c08c174db9ea9cf4eb5a02d08aa9b748
FelipeLara00 <felipe.lara@uc.cl>
2024-05-20 20:32:05 -0400

    arreglo jeraqruia widgets

--- lib/widgets/details_components/okytips.dart
+++ lib/widgets/details_components/okytips.dart
@@ -37,7 +37,7 @@ class _OkyTipsState extends State<OkyTips> {
       }
 
       if (widget.product.containsKey("sabias_que")) {
-        sabiasQue = widget.product["sabias_que"] ?? "Ocurrió un problema inesperado";
+        sabiasQue = widget.product["sabias_que"];
       } else {
         sabiasQue = "No tenemos información de este producto aún :(";
       }
@@ -53,84 +53,94 @@ class _OkyTipsState extends State<OkyTips> {
       color: const Color(0xFFE8E4F4),
       margin: EdgeInsets.symmetric(horizontal: screenWidth * margins),
       padding: const EdgeInsets.all(8),
-      child: Expanded(
-          child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
+      child: Row(
         children: [
-          Container(
-              width: screenWidth * 0.8,
-              padding: const EdgeInsets.all(10.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(15.0),
-              ),
-              child: Column(
-                children: [
-                  Text(
-                    okyTip,
-                    style: const TextStyle(
-                      fontSize: 18,
-                      fontFamily: "Gilroy-Medium",
-                      color: Color(0xFF201547),
-                    ),
-                    textAlign: TextAlign.center,
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: screenWidth * 0.8,
+                  padding: const EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15.0),
                   ),
-                  Align(
-                    alignment: Alignment.bottomRight,
-                    child: Text(
-                      nutricionista,
-                      style: const TextStyle(
-                        fontSize: 16,
-                        fontFamily: "Gilroy-Normal",
-                        color: Color(0xFF7448ED),
+                  child: Column(
+                    children: [
+                      Text(
+                        okyTip,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontFamily: "Gilroy-Medium",
+                          color: Color(0xFF201547),
+                        ),
+                        textAlign: TextAlign.center,
                       ),
-                    ),
-                  ),
-                ],
-              )),
-          Align(
-              alignment: Alignment.bottomRight,
-              child: Padding(
-                  padding: const EdgeInsets.only(right: 40),
-                  child: TriangleWidget())),
-          Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-            Container(
-                width: screenWidth * 0.5,
-                padding: const EdgeInsets.all(
-                    8.0), // Establece un relleno de 5 píxeles alrededor del contenido
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20.0),
+                      Align(
+                        alignment: Alignment.bottomRight,
+                        child: Text(
+                          nutricionista,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontFamily: "Gilroy-Normal",
+                            color: Color(0xFF7448ED),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
                 ),
-                child: Column(
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 40),
+                    child: TriangleWidget()
+                  )
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround, 
                   children: [
-                    const Text(
-                      "Sabías que...",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontFamily: "Gilroy-Bold",
-                        color: Color(0xFF7448ED),
+                    Container(
+                      width: screenWidth * 0.5,
+                      padding: const EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20.0),
                       ),
+                      child: Column(
+                        children: [
+                          const Text(
+                            "Sabías que...",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontFamily: "Gilroy-Bold",
+                              color: Color(0xFF7448ED),
+                            ),
+                          ),
+                          Text(
+                            sabiasQue,
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontFamily: "Gilroy-Medium",
+                              color: Color(0xFF201547),
+                            ),
+                          ),
+                        ],
+                      )
                     ),
-                    Text(
-                      sabiasQue,
-                      textAlign: TextAlign.center,
-                      style: const TextStyle(
-                        fontSize: 16,
-                        fontFamily: "Gilroy-Medium",
-                        color: Color(0xFF201547),
-                      ),
+                    Image.asset(
+                      'lib/assets/nutria_2_sin_cola.png',
+                      height: screenWidth * 0.6,
                     ),
-                  ],
-                )),
-            Image.asset(
-              'lib/assets/nutria_2_sin_cola.png',
-              height: screenWidth * 0.6,
+                  ]
+                )
+              ]
             ),
-          ])
+          )
         ],
-      )),
+      )
     );
   }
 }
