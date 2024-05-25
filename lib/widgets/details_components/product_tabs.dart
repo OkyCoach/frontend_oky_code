da38a9639d6999f55ebe29cb7a794a7fd4e05fd4
FelipeLara00 <felipe.lara@uc.cl>
2024-05-25 17:10:17 -0400

    arreglo texto

--- lib/widgets/details_components/product_tabs.dart
+++ lib/widgets/details_components/product_tabs.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class ProductTabs extends StatelessWidget {
-
   const ProductTabs({
     Key? key,
   }) : super(key: key);
@@ -12,33 +11,50 @@ class ProductTabs extends StatelessWidget {
 
     return Container(
       height: 35,
-      child:TabBar(
+      decoration: const BoxDecoration(
+        border: Border(
+          top: BorderSide(color: Color(0xFF7448ED)),
+          bottom: BorderSide(color: Color(0xFF7448ED)),
+        ),
+      ),
+      child: TabBar(
         indicatorSize: TabBarIndicatorSize.tab,
         indicator: const BoxDecoration(
           color: Color(0xFF76FDB1),
+          border: Border(
+            left: BorderSide(color: Color(0xFF7448ED)),
+            right: BorderSide(color: Color(0xFF7448ED)),
+          ),
         ),
         labelColor: Colors.black,
+        unselectedLabelColor: Colors.black,
         tabs: [
           Tab(
-            child: Text(
-              "Ev. Nutricional",
-              style: TextStyle(
-                fontFamily: "Gilroy-SemiBold",
-                fontSize: screenHeight * 0.02,
+            child: FittedBox(
+              fit: BoxFit.scaleDown,
+              child: Text(
+                "Evaluación Nutri.",
+                style: TextStyle(
+                  fontFamily: "Gilroy-SemiBold",
+                  fontSize: screenHeight * 0.02,
+                ),
               ),
             ),
           ),
           Tab(
-            child: Text(
-              "Oky Tips",
-              style: TextStyle(
-                fontFamily: "Gilroy-SemiBold",
-                fontSize: screenHeight * 0.02,
+            child: FittedBox(
+              fit: BoxFit.scaleDown,
+              child: Text(
+                "Oky Tips",
+                style: TextStyle(
+                  fontFamily: "Gilroy-SemiBold",
+                  fontSize: screenHeight * 0.02,
+                ),
               ),
             ),
           ),
         ],
-      )
+      ),
     );
   }
 }
