5c6c6b68a2cdd49508ca88eafb038f22d0efb2bb
FelipeLara00 <felipe.lara@uc.cl>
2024-06-06 21:34:29 -0400

    refactor perfil

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -19,7 +19,6 @@ class _ProfileState extends State<ProfilePage> {
   @override
   void initState() {
     super.initState();
-    // Initialize the future for user data
     _userDataFuture = AuthManager().getSession();
   }
 
@@ -33,175 +32,178 @@ class _ProfileState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
-    return Scaffold(
-      body: Container(
-        width: screenWidth,
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment.bottomLeft,
-            colors: [
-              Color(0xFF201547),
-              Color(0xFF7448ED),
-            ],
-          ),
-        ),
-        child: FutureBuilder<Map<String, String>>(
-          future: _userDataFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              // Show a loading indicator while waiting for the data
-              return const Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              // If we run into an error, display it to the user
-              return Center(child: Text('Error: ${snapshot.error}'));
-            } else if (snapshot.hasData) {
-              // When we have the data, display it
-              final userData = snapshot.data!;
-              if (userData.containsKey('userInfo') &&
-                  userData['userInfo'] != null) {
-                Map<String, dynamic> userInfo =
-                    json.decode(userData['userInfo']!);
-
-                return Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Center(
-                          child: Image.asset(
-                            'lib/assets/logos/full_logo.png',
-                            width: 200,
-                          )
-                        ),
-                        const Text(
-                          'Tu Perfil:',
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Container(
-                          padding: EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.white), // Añade un borde blanco
-                            borderRadius: BorderRadius.circular(10), // Opcional: Añade bordes redondeados
-                            color: const Color(0xFF7448ED), // Fondo transparente
-                          ),
-                          width: double.infinity,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                '${userInfo['name']} ${userInfo['family_name']}',
-                                style: const TextStyle(
-                                  fontSize: 24,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              
-                              Text(
-                                userInfo['email'],
-                                style: const TextStyle(
-                                  fontSize: 20,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        const Text(
-                          'Acerca de Okylife:',
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Column(
-                          children: [
-                            RichText(
-                              textAlign: TextAlign.justify,
-                              text: TextSpan(
-                                children: [
-                                  const TextSpan(
-                                    text: "- Si deseas saber como respaldamos nuestra evaluacion, te invitamos a ver nuestro ",
-                                    style: TextStyle(
-                                      fontFamily: "Gilroy-Medium",
-                                      fontSize: 18,
-                                      color: const Color(0xFF201547),
-                                    ),
-                                  ),
-                                  TextSpan(
-                                    text: "algoritmo de evaluación de productos.",
-                                    style: const TextStyle(
-                                      decoration: TextDecoration.underline,
-                                      fontSize: 18,
-                                      color: Colors.blue, // Color del enlace
-                                    ),
-                                    recognizer: TapGestureRecognizer()
-                                      ..onTap = () {
-                                        launchUrl(Uri.parse('https://www.okylife.cl/algoritmo/'));
-                                      },
-                                  ),
-                                  
-                                ],
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            const Text(
-                              '- Consulta a un profesional de la salud antes de tomar una decisión sobre tu bienestar.',
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                        ]
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        Center(
-                          child: RoundedButton(
-                            onPressed: _logout,
-                            buttonText: "Cerrar sesión",
-                            size: 200,
-                          )
-                        )
-                        
-                      ],
-                    ));
-              } else {
-                return const Center(
-                  child: Text(
-                    'No se ha iniciado sesión.',
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Expanded(
+          child: SingleChildScrollView(
+            child:  Container(
+              width: screenWidth,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Color(0xFF201547),
+                    Color(0xFF7448ED),
+                  ],
+                ),
+              ),
+              constraints: BoxConstraints(
+                minHeight: MediaQuery.of(context).size.height,
+              ),
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                      child: Image.asset(
+                    'lib/assets/logos/full_logo.png',
+                    width: 200,
+                  )),
+                  const Text(
+                    'Tu Perfil:',
                     style: TextStyle(
-                      fontSize: 20,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
                       color: Colors.white,
                     ),
                   ),
-                );
-              }
-            } else {
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  FutureBuilder<Map<String, String>>(
+                    future: _userDataFuture,
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return CircularProgressIndicator();
+                      } else if (snapshot.hasError) {
+                        return Text('Error: ${snapshot.error}');
+                      } else if (snapshot.hasData) {
+                        final userData = snapshot.data!;
+                        if (userData.containsKey('userInfo') &&
+                            userData['userInfo'] != null) {
+                          Map<String, dynamic> userInfo =
+                              json.decode(userData['userInfo']!);
+                          return Container(
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: Colors.white), // Añade un borde blanco
+                              borderRadius: BorderRadius.circular(
+                                  10), // Opcional: Añade bordes redondeados
+                              color: const Color(0xFF7448ED), // Fondo transparente
+                            ),
+                            width: double.infinity,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  '${userInfo['name']} ${userInfo['family_name']}',
+                                  style: const TextStyle(
+                                    fontSize: 24,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Text(
+                                  userInfo['email'],
+                                  style: const TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        } else {
+                          return Text('No data available');
+                        }
+                        
+                      } else {
+                        return Text('No data available');
+                      }
+                    },
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text(
+                    'Acerca de Okylife:',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Column(
+                    children: [
+                      RichText(
+                        textAlign: TextAlign.justify,
+                        text: TextSpan(
+                          children: [
+                            const TextSpan(
+                              text: "- Si deseas saber como respaldamos nuestra evaluacion, te invitamos a ver nuestro ",
+                              style: TextStyle(
+                                fontFamily: "Gilroy-Medium",
+                                fontSize: 18,
+                                color: const Color(0xFF201547),
+                              ),
+                            ),
+                            TextSpan(
+                              text: "algoritmo de evaluación de productos.",
+                              style: const TextStyle(
+                                decoration: TextDecoration.underline,
+                                fontFamily: "Gilroy-Medium",
+                                fontSize: 18,
+                                color: Colors.blue, // Color del enlace
+                              ),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  launchUrl(Uri.parse('https://www.okylife.cl/algoritmo/'));
+                                },
+                            ),
+                            
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Text(
+                        '- Consulta a un profesional de la salud antes de tomar una decisión sobre tu bienestar.',
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                          fontFamily: "Gilroy-Medium",
+                          fontSize: 18,
+                          color: const Color(0xFF201547),
+                        ),
+                      ),
+                    ]
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Center(
+                    child: RoundedButton(
+                      onPressed: _logout,
+                      buttonText: "Cerrar sesión",
+                      size: 200,
+                    )
+                  )
 
-              return const Center(child: Text('No user data found.'));
-            }
-          },
-        ),
-      ),
+
+
+
+                ],
+              ),
+            ),
+          )
+        )
+      ]
     );
   }
 }
